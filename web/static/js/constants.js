37271d5c2f555fa6b748a683223663e233ccd57e
Brad Johnson <cdale77@gmail.com>
2017-02-12 19:48:37 -0800

    set up flash reducer and actions

--- web/static/js/constants.js
+++ web/static/js/constants.js
@@ -1,5 +1,26 @@
 const Constants = {
-  DEFAULT_STATE: {}
+  DEFAULT_STATE: {
+    flash: {
+      flashType: "",
+      message: ""
+    },
+    session: {
+      signedIn: false,
+      user: {}
+    },
+    routing: {}
+  },
+  ACTIONS: {
+    REQUEST_START:                "REQUEST_START",
+    REQUEST_END:                  "REQUEST_END",
+    FLASH_SUCCESS:                "FLASH_SUCCESS",
+    FLASH_ERROR:                  "FLASH_ERROR",
+    FLASH_CLEAR:                  "FLASH_CLEAR",
+    NEW_SESSION_SUCCESS:          "NEW_SESSION_SUCCESS",
+    NEW_SESSION_FAILURE:          "NEW_SESSION_FAILURE",
+    DESTROY_SESSION_SUCCESS:      "DESTROY_SESSION_SUCCESS",
+    DESTROY_SESSION_FAILURE:      "DESTROY_SESSION_FAILURE",
+  }
 }
 
 export default Constants
