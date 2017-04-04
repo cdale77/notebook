c795d77a53a870a40d156e4b928b4428e9358a89
Brad Johnson <cdale77@gmail.com>
2017-04-03 20:58:49 -0700

    only show note tools if a note is selected

--- web/static/js/components/note_tools.js
+++ web/static/js/components/note_tools.js
@@ -2,9 +2,24 @@ import React                from "react";
 
 export default class NoteTools extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayTools: false
+    }
+  }
+
+  noteToolsClassName() {
+    const base = "note-tools";
+    if (this.props.noteName)
+      return(base);
+    else
+      return(base + " hidden");
+  }
+
   render() {
     return(
-      <div className="note-tools">
+      <div className={this.noteToolsClassName()}>
         <div className="note-buttons">
           <button onClick={this.props.toggleEditor}>
             Edit
